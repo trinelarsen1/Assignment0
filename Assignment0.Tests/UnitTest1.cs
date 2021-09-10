9a7d42c3952322f6018a56717b398722b6f6fdf4
Trine Larsen <tril@itu.dk>
2021-09-10 08:53:37 +0200

    Function and test

--- Assignment0.Tests/UnitTest1.cs
+++ Assignment0.Tests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Assignment0.Tests
 {
-    public class UnitTest1
+    public class Programtests
     {
         [Fact]
         public void Test1()
         {
+            // Arrange 
+            var writer = new StringWriter(); 
+            Console.SetOut(writer);
 
+            //Act
+            Program.Main(new string[0]);
+
+            //Assert
+            var output = writer.GetStringBuilder().ToString();
+            Assert.Equal("Hello, World!", output);
         }
     }
 }
--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Assignment0
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+        static void Main(string[] args){
+            try{
+            Console.WriteLine("Enter year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year > 1582 && IsLeapYear(year) == true){Console.WriteLine("yay");}
+            if (year > 1582 && IsLeapYear(year) == false) {Console.WriteLine("nay");}
+            if (year < 1582) Console.WriteLine("Run the program again and type a year after 1582");
+            } catch (Exception e){Console.WriteLine(e);}
         }
-    }
+
+        static bool IsLeapYear(int year){
+            if ((year % 400) == 0) 
+                return true;
+            else if ((year % 4) == 0) 
+                return true;
+            else if ((year % 100) == 0) 
+                return false;
+            else return false; 
+        }
+    
+
+}
 }
